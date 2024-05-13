47047250f4a098f3ab38db71b7eb083e301f5bdd
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-13 19:02:19 +0200

    update

--- Spiced/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ Spiced/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -18,8 +18,8 @@ const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
+});
 
-  lowercaseButton.addEventListener("click", () => {
-    input.value = input.value.toLowerCase();
-  });
+lowercaseButton.addEventListener("click", () => {
+  input.value = input.value.toLowerCase();
 });
