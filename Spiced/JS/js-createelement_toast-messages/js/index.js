9ac95e5d3b36e3c07ea9e821f35b0a9e33c3b3ce
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-15 11:14:28 +0200

    first task done

--- Spiced/JS/js-createelement_toast-messages/js/index.js
+++ Spiced/JS/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,14 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const newCard = document.createElement("li");
+  newCard.classList.add("toast-container__message");
+  newCard.textContent = "I'm a toast message.";
+
+  toastContainer.append(newCard);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.textContent = "";
 });
