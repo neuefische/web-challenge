f1ad7bd807472e9e16bf52f7792cbb2baefbfe70
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-15 12:45:01 +0200

    2nd task done

--- Spiced/js-createelement_social-media-post/js/index.js
+++ Spiced/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,31 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const createNewPost = () => {
+  const post = document.createElement("article");
+  post.classList.add("post");
+
+  const postContent = document.createElement("p");
+  postContent.classList.add("post__content");
+  postContent.textContent = "This is the content of the new post.";
+
+  const postFooter = document.createElement("footer");
+  postFooter.classList.add("post__footer");
+
+  const usernameSpan = document.createElement("span");
+  usernameSpan.classList.add("post__username");
+  usernameSpan.textContent = "@newuser";
+
+  const newLikeButton = document.createElement("button");
+  newLikeButton.classList.add("post__button");
+  newLikeButton.dataset.js = "like-button";
+  newLikeButton.textContent = "♥ Like";
+  newLikeButton.addEventListener("click", handleLikeButtonClick);
+
+  postFooter.append(usernameSpan, newLikeButton);
+  post.append(postContent, postFooter);
+
+  document.body.append(post);
+};
+
+createNewPost();
