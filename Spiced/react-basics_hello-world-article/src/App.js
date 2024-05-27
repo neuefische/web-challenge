4cd507545bd8667e83000b44bfccd406b18cbed5
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-27 16:03:25 +0200

    fixed errors

--- Spiced/react-basics_hello-world-article/src/App.js
+++ Spiced/react-basics_hello-world-article/src/App.js
@@ -2,19 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
 
 function HelloWorldArticle() {
   return (
     <article>
-    <h1>Hello world!</h1>
-    <p>I'm a paragraph</p>
-    <p>more content</p>
-  </article>
+      <h1>Hello world!</h1>
+      <p>I am a paragraph</p>
+      <p>more content</p>
+    </article>
   );
-}
-
-export default function App() {
-  return <HelloWorldArticle />
 }
