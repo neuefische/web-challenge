97679149be1ea409be31e95301a9b2bb3f29f7b2
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-27 15:41:47 +0200

    first task completed

---
+++ Spiced/react-basics_hello-world/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <div>Say hello...</div>;
+}
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+HelloWorld();
