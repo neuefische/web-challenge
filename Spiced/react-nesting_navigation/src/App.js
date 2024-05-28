4e73febd9611be08caef6e3a77d104053da5b013
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-28 16:37:51 +0200

    third task completed

---
+++ Spiced/react-nesting_navigation/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import { Header } from "./Components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header className="header" />
+      <main>content goes here…</main>
+    </>
+  );
+}
