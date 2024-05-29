8de6a2f76c2199c52ba11f6117ea1924b85e52d2
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-29 11:56:09 +0200

    created an emoji checker which displays if a correct click event sequence has been clicked

---
+++ Spiced/react-state_emoji-checker/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [code, setCode] = useState("");
+
+  const validCode = "🐡🐠🐋";
+
+  const handleClick = (event) => {
+    const newEmoji = event.target.textContent;
+    setCode(code + newEmoji);
+  };
+
+  const handleReset = () => {
+    setCode("");
+  };
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button type="button" onClick={handleClick}>
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button type="button" onClick={handleClick}>
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button type="button" onClick={handleClick}>
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>{" "}
+      </div>
+
+      <button type="button" onClick={handleReset}>
+        Reset
+      </button>
+
+      {code === validCode && <p>Valid code!</p>}
+    </div>
+  );
+}
