4594fc16a645b19cff1dc7cddde9d78ec66d3a81
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-29 15:35:24 +0200

    update

--- Spiced/react-nesting_navigation/src/Components/Header.js
+++ Spiced/react-nesting_navigation/src/Components/Header.js
@@ -1,30 +1,5 @@
-import logo from "../img/logo.jpg";
-import avatar from "../img/avatar.jpg";
+import React from "react";
 
-export function Header() {
-  return (
-    <header>
-      <a href="#">
-        <img className="round-image" src={logo} alt="logo" />
-      </a>
-      <nav>
-        <a className="navigation__link" href="#home">
-          Home
-        </a>
-        <a className="navigation__link" href="#about">
-          About
-        </a>
-        <a className="navigation__link" href="#impressum">
-          Impressum
-        </a>
-      </nav>
-      <button
-        type="button"
-        onClick={() => console.log("I could toggle a profile!")}
-        aria-label="toggle profile"
-      >
-        <img className="round-image" src={avatar} alt="avatar" />
-      </button>
-    </header>
-  );
+export default function Header({ children }) {
+  return <header className="header">{children}</header>;
 }
