2737716469e329b851e46a71037cd8aaed3637c2
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-30 11:57:48 +0200

    used the lifting state up to move the variable into the App.js and render them into child Component using props

---
+++ Spiced/react-state-2_table-reservation/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter.js";
+import "./styles.css";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const handleIncrement = () => setPeople(people + 1);
+  const handleDecrement = () => setPeople(people - 1);
+
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter onIncrement={handleIncrement} onDecrement={handleDecrement} />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
