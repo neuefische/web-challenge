d90ff31cf73b894cac88afcbcff7a3ba971cb4e0
ndrbn0 <andrea.bruni1992@gmail.com>
2024-05-31 16:59:17 +0200

    changed from fetching with useState to useSWR

---
+++ Spiced/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,45 @@
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR from "swr";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+  return response.json();
+};
+
+export default function ISSTracker() {
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <main>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
+      />
+    </main>
+  );
+}
