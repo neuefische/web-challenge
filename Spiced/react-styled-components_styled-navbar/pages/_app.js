42cc5374c82915849a2dfb857be3cddc41c253a1
ndrbn0 <andrea.bruni1992@gmail.com>
2024-06-10 15:50:26 +0200

    created a style for an existing element, within the Component

---
+++ Spiced/react-styled-components_styled-navbar/pages/_app.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import GlobalStyle from "../styles";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  border-bottom: solid 2px grey;
+  padding: 1rem;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <StyledNav>
+        <StyledList>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </StyledList>
+      </StyledNav>
+      <Component {...pageProps} />
+    </>
+  );
+}
