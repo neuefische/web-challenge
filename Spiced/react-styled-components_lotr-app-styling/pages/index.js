141c3e0318fd7b1be5d71a4ffafdf8eb6528ef84
ndrbn0 <andrea.bruni1992@gmail.com>
2024-06-12 10:39:02 +0200

    implemented features

--- Spiced/react-styled-components_lotr-app-styling/pages/index.js
+++ Spiced/react-styled-components_lotr-app-styling/pages/index.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function HomePage() {
   return (
-    <div>
-      <Link href="/volumes">Go to all volumes</Link>
-    </div>
+    <Main>
+      <StyledLink href="/volumes">Go to all volumes</StyledLink>
+    </Main>
   );
 }
+
+const Main = styled.main`
+  display: grid;
+  place-items: center;
+  place-content: center;
+  min-height: 100vh;
+`;
+
+const StyledLink = styled(Link)`
+  color: unset;
+  text-decoration: none;
+  font: var(--font-headline-2);
+`;
